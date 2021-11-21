56013289952dbee0bf293a0049012a6971a5750b
TDCIAN <tdcian71@gmail.com>
2021-11-21 22:41:15 +0900

    setup

---
+++ InfiniteScroll/ContentView.swift
@@ -0,0 +1,17 @@
+//
+//  ContentView.swift
+//  InfiniteScroll
+//
+//  Created by JeongminKim on 2021/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+
---
+++ InfiniteScroll/InfiniteScrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfiniteScrollApp.swift
+//  InfiniteScroll
+//
+//  Created by JeongminKim on 2021/11/21.
+//
+
+import SwiftUI
+
+@main
+struct InfiniteScrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
